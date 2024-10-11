d88c2822118579471ba221bb2db4974e4316d949
Oğuz Can Soyselçuk <cannibal_oguz@hotmail.com>
2024-10-11 21:37:25 +0200

    License added to Tiny.cs

--- TINY/Tiny.cs
+++ TINY/Tiny.cs
@@ -1,6 +1,28 @@
-﻿//TINY, as in, THIS IS NOT YAML.
-//~ is null
-//null is also null but with the implication of missing support from Serializer
+﻿//This is not YAML
+//As of v0.8:
+//MultiDimensional Array's not supported. Dictionary supports built-in value types and string.
+//
+//MIT License
+//
+//Copyright (c) 2023 Oğuz Can Soyselçuk
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
 
 using System;
 using System.Collections;
